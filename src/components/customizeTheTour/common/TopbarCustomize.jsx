1df9d2bcec148177ce4bf7188f9b1780ab8c2dcf
Soundar Rajan <soundar1309@gmail.com>
2024-02-20 16:07:51 +0530

    font changes

--- src/components/customizeTheTour/common/TopbarCustomize.jsx
+++ src/components/customizeTheTour/common/TopbarCustomize.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 const TopbarCustomize = () => {
   return (
-    <div className=" fixed top-0 left-0 lg:left-[19%] xl:left-[22%] right-0 py-2 sm:py-5 space-y-1 font-reem-kufi-fun text-xl sm:text-2xl text-stone-600 bg-[#ebebeb] z-10 px-10">
-      <p>Itenery</p>
-      <div className=" text-base  text-stone-700 font-medium flex items-center gap-3">
+    <div className=" fixed top-0 left-0 lg:left-[19%] xl:left-[22%] right-0 py-2 sm:py-5 space-y-1 font-noto-sans text-xl sm:text-2xl text-stone-600 bg-[#ebebeb] z-10 px-10">
+      <p>Customize your Tour Plan</p>
+      <div className=" text-sm font-Belleza  text-stone-700  font-medium flex items-center gap-3">
         <Link
           to="/"
           className=" hover:underline hover:text-stone-500 duration-300"
